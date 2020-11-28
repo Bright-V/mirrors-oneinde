078379b9827b4b52bd9f17dc58b64af8015858da
vastq <2133270052@qq.com>
2020-11-28 13:59:58 +0800

    index

---
+++ config/base.php
@@ -0,0 +1,114 @@
+<?php return array (
+  'site_name' => 'CloudDrive',
+  'password' => 'Xy251231',
+  'style' => 'classic',
+  'onedrive_root' => '/Mirrors/',
+  'except_path' => 'all',
+  'cache_type' => 'secache',
+  'cache_expire_time' => 120,
+  'cache_refresh_time' => 600,
+  'root_path' => '',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+    ),
+    'video5' => 
+    array (
+      0 => 'mp4',
+      1 => 'webm',
+      2 => 'mkv',
+    ),
+    'video' => 
+    array (
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'mpg',
+      2 => 'mpeg',
+      3 => 'rm',
+      4 => 'rmvb',
+      5 => 'mov',
+      6 => 'wmv',
+      7 => 'asf',
+      8 => 'ts',
+      9 => 'flv',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+    ),
+    'code' => 
+    array (
+      0 => 'html',
+      1 => 'htm',
+      2 => 'php',
+      3 => 'css',
+      4 => 'go',
+      5 => 'java',
+      6 => 'js',
+      7 => 'json',
+      8 => 'txt',
+      9 => 'sh',
+      10 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+  'offline' => 
+  array (
+    'offline' => false,
+    'online' => false,
+    'upload_path' => '/upload/',
+  ),
+  'client_secret' => '3F6Qut5NSa6~wnIimc.3j~w2-03n6.mJ2_',
+  'client_id' => 'f5741dbf-95a4-4ba6-8f2e-09b1af77c857',
+  'redirect_uri' => 'https://vastq.gitee.io/oneindex/',
+  'area' => 'us',
+  'refresh_token' => '0.AAAAwfiFizUEKUyBrd_ltGnKVb8ddPWklaZLjy4Jsa93yFd2ADQ.AgABAAAAAAB2UyzwtQEKR7-rWbgdcBZIAQDs_wIA9P9DMFGmk2lRFK6XGaJiIkiTLHbyiTT_t6gabsXq_UtB938pY2FUd60XTjrm2JscSEx9g-wHe3GghbOtoVWqsYa76orTy13UZHuLXLKaZbH7Ov5W6dEAKzoQSB_81yP8HbDZHEAR6N-Q6kZNHCQsNrtqQuE5X29L6zqL0mK7aik3KW3fj4gaJNVbpZvgbQrmKwbjdLawvNfUEcs6ZnLqJniL5cUOWCeXyqVlBDIeOqmcFYDqmZEYCAqIGAdOEnqw6qNWxm4ysLIv0DDeTt0zRZGXEo-hTw6GqEvgHezrtaBUHmc220ZNJse9XKARC0_TH6JeFtynl_wZk4gaDW9hq9uBxZRQpLiDQNTewX26DPib5X05j85exkIkSdIOtiq6TUHPphAe_0SPmSAgD1wTDpX5lAxPTDLFMVFECOYC33Z8LM4u3G79u1POsuOwbB-iQlI4drjX1IpSOLGPF8NycxR9mIJghFKg2XayeKnGe_3H9On2pNW4WKFCVzScgkqEhXU8u4mFwmrQP0ggWPy9cMYJhso71CLIUsGIkSqrj9MLGU0vyJM0t_WklC2CaSbPi7nzVK3Krk5P0Pru6oAlzPeQKXn9sjChQhUI_Xa4F9XX-NCXvX2sm5A2S2fyg72q7y5ekxUqhkOSoikiVLwCtV7CqkUcHNdN9tyBsXPTMlXh0cSe9PCWl3HE6C64oo0Fv6PGJUD0-nJd7077sTVUbTeV7ejITpdsFP34v4rdZVUZ74-HdmAwEdV6UGRuxsT3d1kS2otA3q9sgb8qq1MrW86cENS2eIsDO-O1JwfmWipdjGtBga7axMxkAcnDkqRQCACP6aIZx-MVS65LlM5fXNQLO3vMFkwOIpa1rfjS2XCoVxG_q24Vn7OGOpvySYigFCwTXL7lI6uMJiD4rwKvFTJ--aVtW95Bxw3XViH8tsRrAuEA61ZrSZnn1oYXWW8G9w',
+  'onedrive_hide' => '',
+  'onedrive_hotlink' => '',
+);
